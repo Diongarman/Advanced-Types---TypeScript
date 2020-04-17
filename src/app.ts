b4daef4f6f0659ad7c2b3a84736e09dc9ba53808
Dion Garman <diongarman@gmail.com>
2020-04-17 14:31:12 +0100

    typeguards

--- src/app.ts
+++ src/app.ts
@@ -24,3 +24,10 @@ type Numeric = number | boolean;
 
 //Intersection SET  is created
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
